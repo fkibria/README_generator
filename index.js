4ebe411e6b1a1ec06bbf701a17a5a2a9e5b2bbec
fkibria <fkibria02@gmail.com>
2021-03-25 13:53:28 -0400

    making template and functions and prompts to fill out

--- index.js
+++ index.js
@@ -1,15 +1,65 @@
+const inquirer = require("inquirer")
+const fs = require("fs")
+const generateMarkdown = require("./utils/generateMarkdown")
+
+
 // array of questions for user
 const questions = [
-
+{
+    type:"input",
+    name:"title",
+    message:"What would you like to call your project?",
+    validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+},
+{
+    type:"input",
+    name:"overview",
+    message:"Describe your project",
+    validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+},
+{
+    type:"input",
+    name:"installation",
+    message:"How do you use your app?",
+    validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+},
+{
+    type:"input",
+    name:"criteria",
+    message:"List criteria for acceptance",
+    validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+},
+{
+    type:"input",
+    name:"license",
+    message:"What license did you use?",
+},
+{
+    type:"input",
+    name:"deployed",
+    message:"Please include link to deployed app",
+    validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+},
+{
+    type:"input",
+    name:"github",
+    message:"Enter your github",
+    validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+},
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(data) {
+    fs.writeFileSync("newREADME.md", data)
+    console.log("success!")
 }
 
 // function to initialize program
 function init() {
-
+inquirer.prompt(questions)
+.then((response)=>{
+writeToFile(generateMarkdown(response))
+})
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,26 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+### Table of Contents\n\n- [Overview](#overview)\n- [Link to App](#deployed)\n- [Installation](#installation)\n- [Criteria](#criteria)\n- [License](#license)\n- [Github](#github)\n-  
+## Overview
+${data.overview}
+
+## Link to App 
+${data.deployed}
+
+## Installation
+
+${data.installation}
+
+## Acceptance Criteria
+${data.criteria}
+
+## License
+${data.license}
+
+## Github
+${data.github}
+
 `;
 }
 
